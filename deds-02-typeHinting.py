1c3d97fdf1a02fac5cc065dc64e27155aa45ce5a
Taufik Sutanto <taufikedys@gmail.com>
2026-03-31 06:04:28 +0700

    week-03

---
+++ deds-02-typeHinting.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 05:51:04 2026
+
+@author: taufi
+"""
+
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+result: int = add_numbers(5, 3)
+print(result)
